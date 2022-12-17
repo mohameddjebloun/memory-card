5ab83ebf1b3967da80fa204afd37fa709f409ce2
Mohamed Djebloun <djeblounm@gmail.com>
2022-12-17 13:21:03 +0100

    Initialize the score states

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,6 +1,20 @@
+//Imports
+import { useState } from "react";
 import PlayersList from "./PlayersList";
-
+import Score from "./Score";
+//Component function
 const Body = () => {
-    return <PlayersList />;
+    //Initialize the states
+    const [currentScore, setCurrentScore] = useState(0);
+    const [bestScore, setBestScore] = useState(0);
+    return (
+        <>
+            <Score currentScore={currentScore} bestScore={bestScore} />
+            <PlayersList
+                changeCurrentScore={setCurrentScore}
+                changeBestScore={setBestScore}
+            />
+        </>
+    );
 };
 export default Body;
